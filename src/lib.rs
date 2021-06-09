46ea41c857690ebf7232766ff747ca528c6b1858
Alkajato <Alkajatomota@gmail.com>
2021-06-09 16:45:53 -0400

    Increased performance with refactor.

--- src/lib.rs
+++ src/lib.rs
@@ -11,19 +11,17 @@ pub trait Organism {
 
 /// Calls calculate_fitness(), mate(), then mutate() accordingly to improve overall fitness.
 pub fn evolve<T: Organism + Send + Sync>(population: &mut [T]) {
-    let scores = population
-        .par_iter()
-        .map(|item| item.calculate_fitness())
-        .collect::<Vec<f64>>();
     let mut mated: Vec<bool> = vec![false; population.len()];
 
     for i in 0..population.len() - 2 {
         if let [previous, current, next, ..] = &mut population[i..] {
-            if previous.calculate_fitness() >= scores[i + 1] {
-                current.mate(previous);
+            let current_score = current.calculate_fitness();
+
+            if previous.calculate_fitness() >= current_score {
+                current.mate(&previous);
                 mated[i + 1] = true;
-            } else if scores[i + 1] <= scores[i + 2] {
-                current.mate(next);
+            } else if current_score <= next.calculate_fitness() {
+                current.mate(&next);
                 mated[i + 1] = true;
             }
         }
