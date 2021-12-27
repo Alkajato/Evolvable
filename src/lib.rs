fc80a5891f3203d2b8fe7ae1c3e1a8aaf71b66ba
Alkajato <Alkajatomota@gmail.com>
2021-12-26 22:07:34 -0500

    Flattened code and removed unneeded function.

--- src/lib.rs
+++ src/lib.rs
@@ -9,84 +9,46 @@ pub trait Organism {
     fn mutate(&mut self);
 }
 
-/// Calls calculate_fitness(), mate(), then mutate() accordingly to improve overall fitness.
-fn process<T: Organism + Send + Sync>(population: &mut [T]) {
-    /// Returns the indices before and after index in items--loops around if out of bounds.
-    fn get_neighbors<T>(items: &[T], index: usize) -> (usize, usize) {
-        let mut indices = (index - 1, index + 1);
+/// Returns the indices before and after index in items--loops around if out of bounds.
+fn get_neighbors<T>(items: &[T], index: usize) -> (usize, usize) {
+    let mut indices = (index - 1, index + 1);
 
-        if index == 0 {
-            indices.0 = items.len() - 1;
-        }
-
-        if index == items.len() - 1 {
-            indices.1 = 0;
-        }
-
-        indices
+    if index == 0 {
+        indices.0 = items.len() - 1;
     }
 
-    /// Returns three references from items, left, center, and right of `items[index]`.
-    /// Loops around if `index == 0` or `index == items.len() - 1`
-    fn get_three<T>(items: &mut [T], index: usize) -> (&T, &mut T, &T) {
-        if index == 0 {
-            let (behind, ahead) = items.split_at_mut(1);
-
-            (&ahead[ahead.len() - 1], &mut behind[0], &ahead[0])
-        } else if index == items.len() - 1 {
-            let (behind, ahead) = items.split_at_mut(index);
-
-            (&behind[behind.len() - 1], &mut ahead[0], &behind[0])
-        } else {
-            let (behind, ahead) = items.split_at_mut(index);
-            let (center, ahead) = ahead.split_at_mut(1);
-
-            (&behind[behind.len() - 1], &mut center[0], &ahead[0])
-        }
+    if index == items.len() - 1 {
+        indices.1 = 0;
     }
 
-    let len = population.len();
+    indices
+}
 
-    let scores: Vec<f64> = population
-        .iter()
-        .map(|item| item.calculate_fitness())
-        .collect();
+/// Returns three references from items, left, center, and right of `items[index]`.
+/// Loops around if `index == 0` or `index == items.len() - 1`
+fn get_three<T>(items: &mut [T], index: usize) -> (&T, &mut T, &T) {
+    if index == 0 {
+        let (behind, ahead) = items.split_at_mut(1);
 
-    let mated: Vec<bool> = scores
-        .iter()
-        .enumerate()
-        .map(|(index, current_score)| {
-            let (prev_score, next_score) = get_neighbors(&scores, index);
+        (&ahead[ahead.len() - 1], &mut behind[0], &ahead[0])
+    } else if index == items.len() - 1 {
+        let (behind, ahead) = items.split_at_mut(index);
 
-            if current_score <= &scores[prev_score] && current_score <= &scores[next_score] {
-                true
-            } else {
-                false
-            }
-        })
-        .collect();
+        (&behind[behind.len() - 1], &mut ahead[0], &behind[0])
+    } else {
+        let (behind, ahead) = items.split_at_mut(index);
+        let (center, ahead) = ahead.split_at_mut(1);
 
-    for i in 0..len {
-        if mated[i] {
-            let (before, after) = get_neighbors(&scores, i);
-
-            let (behind, current, ahead) = get_three(population, i);
-            if scores[before] > scores[after] {
-                current.mate(behind);
-            } else {
-                current.mate(ahead);
-            }
-
-            current.mutate();
-        }
+        (&behind[behind.len() - 1], &mut center[0], &ahead[0])
     }
 }
 
 /// Calls calculate_fitness(), mate(), then mutate() accordingly to improve overall fitness.
-pub fn evolve<T: Organism + Send + Sync>(population: &mut [T]) {
+pub fn evolve<T: Organism + Send + Sync>(input: &mut [T]) {
+    // Make cores number of splits on input to process in parallel.
     let cores = num_cpus::get();
 
-    let mut splits: Vec<&mut [T]> = vec![population];
+    let mut splits: Vec<&mut [T]> = vec![input];
     for _ in 0..cores / 2 {
         let mut temp = Vec::new();
 
@@ -99,9 +61,45 @@ pub fn evolve<T: Organism + Send + Sync>(population: &mut [T]) {
         splits = temp;
     }
 
-    splits.par_iter_mut().for_each(|item| process(*item));
+    splits.par_iter_mut().for_each(|population| {
+        let len = population.len();
+
+        let scores: Vec<f64> = population
+            .iter()
+            .map(|item| item.calculate_fitness())
+            .collect();
+
+        let mated: Vec<bool> = scores
+            .iter()
+            .enumerate()
+            .map(|(index, current_score)| {
+                let (prev_score, next_score) = get_neighbors(&scores, index);
+
+                if current_score <= &scores[prev_score] && current_score <= &scores[next_score] {
+                    true
+                } else {
+                    false
+                }
+            })
+            .collect();
+
+        for i in 0..len {
+            if mated[i] {
+                let (before, after) = get_neighbors(&scores, i);
+
+                let (behind, current, ahead) = get_three(*population, i);
+                if scores[before] > scores[after] {
+                    current.mate(behind);
+                } else {
+                    current.mate(ahead);
+                }
+
+                current.mutate();
+            }
+        }
+    });
     // Due to splitting_at_mut, genes would not travel across the entire slice without rotation.
-    population.rotate_left(1);
+    input.rotate_left(1);
 }
 
 /// Returns the best Organism struct from Population.
