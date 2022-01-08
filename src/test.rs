c7a8dff5e6a815175606db5c28330de88efc4c4e
Alkajato <Alkajatomota@gmail.com>
2022-01-08 18:08:45 -0500

    Added test for get_chunk_size

--- src/test.rs
+++ src/test.rs
@@ -2,6 +2,7 @@ use rand::{thread_rng, Rng};
 use std::fmt;
 
 use crate::evolve;
+use crate::get_chunk_size;
 use crate::Organism;
 
 use self::test_file_writer::StopWatch;
@@ -88,4 +89,28 @@ fn test_evolve() {
     }
 
     writer.make_results("time_evolve().txt");
+}
+
+#[test]
+fn test_get_chunk_size() {
+    let cores = num_cpus::get();
+
+    for len in cores..=100 {
+        let bools = vec![false; len];
+
+        let chunk_size = get_chunk_size(bools.len());
+
+        println!("\nSize: {} chunk_size: {}", len, chunk_size);
+        for chunk in bools.chunks(chunk_size) {
+            println!("\tSize of Chunk: {}", chunk.len());
+
+            assert!(
+                chunk.len() >= 3,
+                "Size: {}. chunk_size (of {}) produced invalid chunk len: {}",
+                len,
+                chunk_size,
+                chunk.len()
+            );
+        }
+    }
 }
