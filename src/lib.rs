45a1f65702a2ee36d22cc46383e5fc79776bbecf
Alkajato <Alkajatomota@gmail.com>
2022-01-08 18:07:57 -0500

    Calculate chunk_size properly to not have < 3 chunk sizes

--- src/lib.rs
+++ src/lib.rs
@@ -29,6 +29,46 @@ fn get_three<T>(input: &mut [T], index: usize) -> (&T, &mut T, &T) {
     return (&behind[behind.len() - 1], &mut center[0], &ahead[0]);
 }
 
+// Minimal amount of chunks that doesn't end with the last chunk being < 3
+fn get_chunk_size(len: usize) -> usize {
+    let cores = num_cpus::get();
+
+    let mut chunk_size = len / cores;
+    while len % chunk_size < 3 {
+        if len % chunk_size == 0 && chunk_size >= 3 {
+            return chunk_size;
+        }
+
+        chunk_size += 1;
+    }
+
+    chunk_size
+}
+
+#[test]
+fn test_get_chunk_size() {
+    let cores = num_cpus::get();
+
+    for len in cores..=100 {
+        let bools = vec![false; len];
+
+        let chunk_size = get_chunk_size(bools.len());
+
+        println!("\nSize: {} chunk_size: {}", len, chunk_size);
+        for chunk in bools.chunks(chunk_size) {
+            println!("\tSize of Chunk: {}", chunk.len());
+
+            assert!(
+                chunk.len() >= 3,
+                "Size: {}. chunk_size (of {}) produced invalid chunk len: {}",
+                len,
+                chunk_size,
+                chunk.len()
+            );
+        }
+    }
+}
+
 /// Calls calculate_fitness(), mate(), then mutate() accordingly to improve overall fitness.
 pub fn evolve<T: Organism + Send + Sync>(input: &mut [T]) {
     let mut scores: Vec<f64> = input
@@ -37,8 +77,7 @@ pub fn evolve<T: Organism + Send + Sync>(input: &mut [T]) {
         .collect();
 
     // Split the input evenly to work on in parallel.
-    let cores = num_cpus::get();
-    let chunk_size = input.len() / cores + (input.len() % (input.len() / cores));
+    let chunk_size = get_chunk_size(input.len());
 
     input
         .par_chunks_mut(chunk_size)
