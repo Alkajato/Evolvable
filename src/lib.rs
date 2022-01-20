a657465bc151557f8e258b9be4b92de905f0da3c
Alkajato <Alkajatomota@gmail.com>
2022-01-20 12:26:54 -0500

    get_three no longer wraps around.

--- src/lib.rs
+++ src/lib.rs
@@ -14,13 +14,13 @@ fn get_three<T>(input: &mut [T], index: usize) -> (&T, &mut T, &T) {
     if index == 0 {
         let (behind, ahead) = input.split_at_mut(1);
 
-        return (&ahead[ahead.len() - 1], &mut behind[0], &ahead[0]);
+        return (&ahead[1], &mut behind[0], &ahead[0]);
     }
 
     if index == input.len() - 1 {
         let (behind, ahead) = input.split_at_mut(index);
 
-        return (&behind[behind.len() - 1], &mut ahead[0], &behind[0]);
+        return (&behind[behind.len() - 1], &mut ahead[0], &behind[behind.len() - 2]);
     }
 
     let (behind, ahead) = input.split_at_mut(index);
@@ -32,41 +32,17 @@ fn get_three<T>(input: &mut [T], index: usize) -> (&T, &mut T, &T) {
 // Minimal amount of chunks that doesn't end with the last chunk being < 3
 fn get_chunk_size(len: usize) -> usize {
     let cores = num_cpus::get();
-
+    
     let mut chunk_size = len / cores;
     while len % chunk_size < 3 {
         if len % chunk_size == 0 && chunk_size >= 3 {
-            return chunk_size;
+            return chunk_size;    
         }
-
+        
         chunk_size += 1;
     }
 
     chunk_size
-}
-
-#[test]
-fn test_get_chunk_size() {
-    let cores = num_cpus::get();
-
-    for len in cores..=100 {
-        let bools = vec![false; len];
-
-        let chunk_size = get_chunk_size(bools.len());
-
-        println!("\nSize: {} chunk_size: {}", len, chunk_size);
-        for chunk in bools.chunks(chunk_size) {
-            println!("\tSize of Chunk: {}", chunk.len());
-
-            assert!(
-                chunk.len() >= 3,
-                "Size: {}. chunk_size (of {}) produced invalid chunk len: {}",
-                len,
-                chunk_size,
-                chunk.len()
-            );
-        }
-    }
 }
 
 /// Calls calculate_fitness(), mate(), then mutate() accordingly to improve overall fitness.
