6ccd4de87a6f2aee919291b1cfa3e18a513b2037
Alkajato <Alkajatomota@gmail.com>
2022-03-21 05:41:28 -0400

    Reworked evolve() to massively simplify code and reduce tests.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use rayon::prelude::*;
+use rand::{thread_rng, prelude::SliceRandom};
+use rayon::{prelude::*, iter::Either};
 
 pub trait Organism {
     /// Calculate and return a fitness score using this.
@@ -9,80 +10,47 @@ pub trait Organism {
     fn mutate(&mut self);
 }
 
-/// Returns three references from input, left, center, and right of `items[index]`.
-fn get_three<T>(input: &mut [T], index: usize) -> (&T, &mut T, &T) {
-    if index == 0 {
-        let (behind, ahead) = input.split_at_mut(1);
-
-        return (&ahead[1], &mut behind[0], &ahead[0]);
-    }
-
-    if index == input.len() - 1 {
-        let (behind, ahead) = input.split_at_mut(index);
-
-        return (&behind[behind.len() - 1], &mut ahead[0], &behind[behind.len() - 2]);
-    }
-
-    let (behind, ahead) = input.split_at_mut(index);
-    let (center, ahead) = ahead.split_at_mut(1);
-
-    return (&behind[behind.len() - 1], &mut center[0], &ahead[0]);
-}
-
-// Minimal amount of chunks that doesn't end with the last chunk being < 3
-fn get_chunk_size(len: usize) -> usize {
-    let cores = num_cpus::get();
-    
-    let mut chunk_size = len / cores;
-    while len % chunk_size < 3 {
-        if len % chunk_size == 0 && chunk_size >= 3 {
-            return chunk_size;    
-        }
-        
-        chunk_size += 1;
-    }
-
-    chunk_size
-}
-
 /// Calls calculate_fitness(), mate(), then mutate() accordingly to improve overall fitness.
 pub fn evolve<T: Organism + Send + Sync>(input: &mut [T]) {
-    let mut scores: Vec<f64> = input
+    let scores: Vec<f64> = input
         .par_iter()
         .map(|item| item.calculate_fitness())
         .collect();
 
-    // Split the input evenly to work on in parallel.
-    let chunk_size = get_chunk_size(input.len());
+    let len = input.len();
+    let mating_pool: Vec<bool> = (0..len)
+        .into_par_iter()
+        .map(|i| {
+            let left = if i > 0 { i - 1 } else { len - 1 };
+            let right = if i < len - 1 { i + 1 } else { 0 };
 
-    input
-        .par_chunks_mut(chunk_size)
-        .zip(scores.par_chunks_mut(chunk_size))
-        .for_each(|(chunk, scores)| {
-            let len = chunk.len();
+            scores[left] < scores[i] || scores[i] > scores[right]
+        })
+        .collect();
 
-            for i in 0..len {
-                let (previous_score, current_score, next_score) = get_three(scores, i);
-
-                if *current_score <= *previous_score && *current_score <= *next_score {
-                    let (behind, current, ahead) = get_three(chunk, i);
-
-                    if previous_score > next_score {
-                        current.mate(behind);
-                    } else {
-                        current.mate(ahead);
-                    }
-
-                    current.mutate();
-                }
+    let (mating_pool, mut replacing): (Vec<&T>, Vec<&mut T>) = input
+        .par_iter_mut()
+        .zip(mating_pool)
+        .partition_map(|(member, mated)| {
+            if mated {
+                Either::Left(&*member)
+            } else {
+                Either::Right(member)
             }
         });
 
-    // Since iterating over chunks do not overlap, genes would not travel across the entire slice without rotation.
-    input.rotate_left(1);
+    replacing
+        .par_iter_mut()
+        .for_each_init(thread_rng, |rng, member| {
+            if let Some(mate) = mating_pool.choose(rng) {
+                member.mate(mate);
+            }
+
+            member.mutate();
+        });
 }
 
-/// Returns the best Organism struct from Population.
+/// Returns the best Organism struct from population.
 pub fn get_best<T: Organism + Send + Sync>(population: &[T]) -> &T {
     population
         .par_iter()
@@ -94,6 +62,14 @@ pub fn get_best<T: Organism + Send + Sync>(population: &[T]) -> &T {
             }
         })
         .expect("Population failed to yield best from get_best()")
+}
+
+/// Returns the average fitness score from population.
+pub fn get_average<T: Organism + Send + Sync>(population: &[T]) -> f64 {
+    population
+        .par_iter()
+        .map(|item| item.calculate_fitness())
+        .sum::<f64>() / (population.len() as f64)
 }
 
 // Referring to test.rs for separate tests file.
--- src/test.rs
+++ src/test.rs
@@ -1,43 +1,17 @@
-use rand::prelude::SliceRandom;
-use rand::{thread_rng, Rng};
-use rayon::iter::*;
-
 use self::test_file_writer::StopWatch;
-use crate::evolve;
-use crate::get_chunk_size;
-use crate::Organism;
 use crate::test::test_types::*;
+use crate::{evolve, get_average};
 
-mod test_types;
 mod test_file_writer;
-
-fn get_average<T: Organism + Send + Sync>(population: &[T]) -> f64 {
-    let sum: f64 = population
-        .par_iter()
-        .map(|item| item.calculate_fitness())
-        .sum();
-    sum / (population.len() as f64)
-}
+mod test_types;
 
 const ITERATIONS: std::ops::Range<i32> = 0..24;
 const POP_SIZE: usize = 5_000_000; // Normal value is 5_000_000
 
 #[test]
 fn test_evolve() {
-    let algorithm = evolve;
-
     let mut population: Vec<EvNum> = make_ev_nums(POP_SIZE);
-
-    let previous_average = get_average(&population);
-    algorithm(&mut population);
-    let current_average = get_average(&population);
-
-    assert!(
-        current_average > previous_average,
-        "C_Avg: {}, P_Avg: {}",
-        current_average,
-        previous_average
-    );
+    // let mut population: Vec<i32> = vec![0; POP_SIZE];
 
     let mut improvement = 0.0;
     let mut writer = StopWatch::new();
@@ -45,10 +19,17 @@ fn test_evolve() {
         let previous_average = get_average(&population);
 
         writer.start();
-        algorithm(&mut population);
+        evolve(&mut population);
         writer.lap();
 
         let current_average = get_average(&population);
+
+        assert!(
+            current_average > previous_average,
+            "C_Avg: {}, P_Avg: {}",
+            current_average,
+            previous_average
+        );
 
         let percent_improved = ((current_average / previous_average) * 100.0) - 100.0;
         improvement += percent_improved;
@@ -58,68 +39,4 @@ fn test_evolve() {
     let average_improvement = String::from(format!("Average Improvement:\n{improvement}%"));
 
     writer.make_results("time_evolve().txt", Some(average_improvement));
-}
-
-#[test]
-fn test_get_chunk_size() {
-    let cores = num_cpus::get();
-
-    for len in cores..=100 {
-        let bools = vec![false; len];
-
-        let chunk_size = get_chunk_size(bools.len());
-
-        println!("\nSize: {} chunk_size: {}", len, chunk_size);
-        for chunk in bools.chunks(chunk_size) {
-            println!("\tSize of Chunk: {}", chunk.len());
-
-            assert!(
-                chunk.len() >= 3,
-                "Size: {}. chunk_size (of {}) produced invalid chunk len: {}",
-                len,
-                chunk_size,
-                chunk.len()
-            );
-        }
-    }
-}
-
-fn evolve_new<T: Organism + Send + Sync>(input: &mut [T]) {
-    let scores: Vec<f64> = input
-        .par_iter()
-        .map(|item| item.calculate_fitness())
-        .collect();
-
-    let len = scores.len();
-    let mating_pool: Vec<bool> = (0..len)
-        .into_par_iter()
-        .map(|i| {
-            let left = if i > 0 { i - 1 } else { len - 1 };
-            let right = if i < len - 1 { i + 1 } else { 0 };
-
-            scores[left] < scores[i] || scores[i] > scores[right]
-        })
-        .collect();
-
-    let (mating_pool, mut replacing): (Vec<&T>, Vec<&mut T>) = input
-        .par_iter_mut()
-        .zip(mating_pool)
-        .partition_map(|(member, mated)| {
-            if mated {
-                Either::Left(&*member)
-            } else {
-                Either::Right(member)
-            }
-        });
-
-    replacing.par_iter_mut().for_each_init(
-        || thread_rng(),
-        |rng, member| {
-            if let Some(mate) = mating_pool.choose(rng) {
-                member.mate(&mate);
-            }
-
-            member.mutate();
-        },
-    );
 }
