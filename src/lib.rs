6f84beb48d298712c48b9e4bd11b7e60140f599d
Alkajato <Alkajatomota@gmail.com>
2023-10-12 15:44:23 -0400

    Breaking API changes, cache scores instead of recalculation each call.

--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,73 @@
-use rayon::prelude::{
-    FromParallelIterator, IndexedParallelIterator, IntoParallelIterator, IntoParallelRefIterator,
-    IntoParallelRefMutIterator, ParallelIterator,
-};
+use rayon::prelude::{IntoParallelIterator, IntoParallelRefIterator, ParallelIterator};
 
-/// Parallel naiive genetic algorithm that uses crossover and mutation operators.
-/// Cross overs mutate the first arg to cross over with the read-only reference arg.
-pub fn evolve<T: Send + Sync, U: PartialOrd + Send + Sync>(
-    population: &mut [T],
-    score: impl Fn(&T) -> U + Send + Sync,
-    cross_over: impl Fn(&mut T, &T) + Send + Sync,
-    mutate: impl Fn(&mut T) + Send + Sync,
-) {
-    let len = population.len();
-    if len == 0 {
-        return;
-    }
-
-    let scores = Vec::from_par_iter(population.par_iter().map(score));
-
-    // Culled if equal to a neighbor and under another,
-    // under both neighbors, or equal both neighbors.
-    let (culled, survive): (Vec<_>, Vec<_>) =
-        population.par_iter_mut().enumerate().partition(|(i, _)| {
-            let (prev, next) = neighbors(*i, len);
-            scores[prev] >= scores[*i] && scores[*i] <= scores[next]
-        });
-
-    culled.into_par_iter().for_each(|(i, change)| {
-        let (prev, next) = neighbors(i, len);
-
-        let mut compare = next;
-        if scores[prev] > scores[next] {
-            compare = prev;
-        }
-
-        if let Ok(index) = survive.binary_search_by(|(x, _)| x.cmp(&compare)) {
-            cross_over(change, survive[index].1);
-        }
-
-        mutate(change);
-    });
+pub trait Evolvable
+where
+    Self: Send + Sync,
+{
+    fn mate(&mut self, mate: &Self);
+    fn score(&self) -> f32;
+    fn mutate(&mut self);
 }
 
-/// Wraps around borders, gets "left" and "right" of `index`.
-/// Panics in debug if `len` is `0`.
-fn neighbors(index: usize, len: usize) -> (usize, usize) {
-    let prev = (index + len - 1) % len;
-    let next = (index + 1) % len;
-    (prev, next)
+pub struct Evolver<T: Evolvable> {
+    pub scores: Vec<f32>,
+    population: Vec<T>,
+}
+
+impl<T: Evolvable> Evolver<T> {
+    pub fn new(population: Vec<T>) -> Self {
+        let cpu_count = num_cpus::get();
+        let len = population.len();
+
+        let min_safe_size = cpu_count * 2;
+        assert!(len >= min_safe_size, "Unexpectedly small population size may enable rare memory safety violations. Expected at least more than {min_safe_size}, got {len}");
+
+        let scores = population.par_iter().map(Evolvable::score).collect();
+        Evolver { scores, population }
+    }
+
+    pub fn len(&self) -> usize {
+        self.population.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.population.len() == 0
+    }
+
+    /// Parallel naiive genetic algorithm that uses crossover and mutation operators.
+    /// Cross overs mutate the first arg to cross over with the read-only reference arg.
+    #[allow(invalid_reference_casting)]
+    pub fn evolve(&mut self) {
+        let len = self.scores.len();
+
+        (0..len).into_par_iter().for_each(|i| {
+            let left = if i > 0 { i - 1 } else { len - 1 };
+            let right = if i < len - 1 { i + 1 } else { 0 };
+
+            unsafe {
+                let score = &self.scores[i];
+                let score_left = &self.scores[left];
+                let score_right = &self.scores[right];
+
+                if score_left >= score && score <= score_right {
+                    let mut mate = right;
+                    if score_left > score_right {
+                        mate = left;
+                    }
+
+                    // If one neighbor is worse then population[i] is not mut referenced.
+                    // So at any given index if we mutate we do not have writes happening to our neighbors.
+                    // Therefore this is safe across threads because at worst multiple threads will only read-access the same thing.
+                    let current = (&self.population[i]) as *const T as *mut T;
+                    (*current).mate(self.population.get_unchecked(mate));
+                    (*current).mutate();
+
+                    let ptr = score as *const f32 as *mut f32;
+                    *ptr = (*current).score();
+                }
+            }
+        });
+    }
 }
 
 // Referring to test.rs for separate tests file.
--- src/test.rs
+++ src/test.rs
@@ -1,41 +1,62 @@
-use rand::{thread_rng, Rng};
+use rand::{seq::SliceRandom, thread_rng, Rng};
 use rayon::prelude::{
     FromParallelIterator, IntoParallelIterator, IntoParallelRefIterator, ParallelIterator,
 };
 use std::time::Instant;
 
-use crate::evolve;
+use crate::{Evolvable, Evolver};
 
-const ITERATIONS: std::ops::Range<i32> = 0..50;
+const ITERATIONS: std::ops::Range<i32> = 0..800;
 const POP_SIZE: usize = 5_000_000; // Normal value is 5_000_000
 
+#[derive(Debug)]
+struct EvolvableNum(i32);
+impl Evolvable for EvolvableNum {
+    fn mate(&mut self, mate: &Self) {
+        self.0 = ((self.0 as f32 + mate.0 as f32) / 2.0).round() as i32;
+    }
+
+    fn score(&self) -> f32 {
+        self.0 as f32
+    }
+
+    fn mutate(&mut self) {
+        self.0 += *[-1, 1].choose(&mut thread_rng()).unwrap();
+    }
+}
+
+// Avg time: 0.02295171326658325
 #[test]
 fn profile() {
-    let mut population = Vec::from_par_iter(
+    let population: Vec<EvolvableNum> = Vec::from_par_iter(
         (0..POP_SIZE)
             .into_par_iter()
-            .map(|_| thread_rng().gen_range(-50..50)),
+            .map(|_| EvolvableNum(thread_rng().gen_range(-50..50))),
     );
 
-    let score = |x: &i32| *x;
-    let cross_over = |x: &mut i32, y: &i32| *x = ((*x as f32 + *y as f32) / 2.0).round() as i32;
-    let mutate = |x: &mut i32| unsafe {
-        let nums = [-1, 1];
-        *x += nums.get_unchecked(thread_rng().gen_range(0..nums.len()));
-    };
+    let mut optimizer = Evolver::new(population);
 
     let mut total_time = 0.0;
     for count in ITERATIONS {
-        let previous_average: f32 =
-            population.par_iter().map(|x| *x as f32).sum::<f32>() / POP_SIZE as f32;
+        let previous_average: f32 = optimizer
+            .population
+            .par_iter()
+            .map(|x| x.0 as f32)
+            .sum::<f32>()
+            / POP_SIZE as f32;
 
         let start = Instant::now();
-        evolve(&mut population, score, cross_over, mutate);
+        optimizer.evolve();
+        // evolve(&mut population, score, cross_over, mutate);
         let time = start.elapsed().as_secs_f64();
         total_time += time;
 
-        let current_average: f32 =
-            population.par_iter().map(|x| *x as f32).sum::<f32>() / POP_SIZE as f32;
+        let current_average: f32 = optimizer
+            .population
+            .par_iter()
+            .map(|x| x.0 as f32)
+            .sum::<f32>()
+            / POP_SIZE as f32;
 
         assert!(
             current_average > previous_average,
@@ -56,22 +77,17 @@ fn profile() {
 // Observe changes to fitness scores.
 #[test]
 fn observe() {
-    let mut population = Vec::from_par_iter(
-        (0..10)
+    let population = Vec::from_par_iter(
+        (0..12)
             .into_par_iter()
-            .map(|_| thread_rng().gen_range(-50..50)),
+            .map(|_| EvolvableNum(thread_rng().gen_range(-50..50))),
     );
 
-    let score = |x: &i32| *x;
-    let cross_over = |x: &mut i32, y: &i32| *x = ((*x as f32 + *y as f32) / 2.0).round() as i32;
-    let mutate = |x: &mut i32| unsafe {
-        let nums = [-1, 1];
-        *x += nums.get_unchecked(thread_rng().gen_range(0..nums.len()));
-    };
+    let mut optimizer = Evolver::new(population);
 
     for _ in 0.. {
-        evolve(&mut population, score, cross_over, mutate);
-        eprint!("\x1b[2K{population:?}\r");
+        optimizer.evolve();
+        eprint!("\x1b[2K{:?}\r", optimizer.population);
 
         std::thread::sleep(std::time::Duration::from_secs_f32(0.7));
     }
